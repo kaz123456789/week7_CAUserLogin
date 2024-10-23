7d1c3dd56635166bfd48ee450551dd9107096d5a
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2024-10-23 15:17:05 -0400

    Task2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set current user to the input name.
+     * @param name the name
+     */
+    void setCurrentUser(String name);
 }
